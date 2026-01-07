3d977045ffcf7873e8165c54f88792572851de6a
newest-resu <cetin.mustf@gmail.com>
2026-01-07 15:26:06 +0300

    Update generator.py

--- news/generator.py
+++ news/generator.py
@@ -14,19 +14,58 @@ RSS_FEEDS = [
     ("Al Jazeera", "https://www.aljazeera.com/xml/rss/all.xml"),
 ]
 
-LOCAL_KEYWORDS = [
-    "belediye", "valilik", "kaymakam",
-    "istanbul", "bursa", "kocaeli", "sakarya", "yalova"
-]
+INTL_CATEGORY_KEYWORDS = {
+    "dunya": ["war", "conflict", "attack", "peace", "border", "un", "nato"],
+    "ekonomi": ["economy", "inflation", "market", "bank", "oil", "gas"],
+    "savunma": ["military", "army", "defense", "missile", "weapon"],
+    "teknoloji": ["ai", "artificial intelligence", "tech", "google", "apple"],
+    "spor": ["match", "goal", "league", "tournament"]
+}
 
 CATEGORY_KEYWORDS = {
-    "yerel": LOCAL_KEYWORDS,
-    "gundem": ["bakan", "meclis", "cumhurbaşkanı", "seçim"],
-    "dunya": ["ukraine", "israel", "gaza", "usa", "china", "russia"],
-    "spor": ["maç", "transfer", "gol", "lig"],
-    "ekonomi": ["enflasyon", "dolar", "borsa", "faiz"],
-    "teknoloji": ["yapay zeka", "ai", "apple", "google", "tesla"],
-}
+    "yerel": ["belediye", "valilik", "kaymakam","istanbul", "bursa", "kocaeli", "sakarya", "yalova","çınarcık","cinarcik","çiftlikköy","ciftlikkoy","termal","altınova","altinova","armutlu",
+        "yalova belediyesi","yalova valiliği","yalova valiligi","il genel meclisi",
+        "imar","altyapı","altyapi","su kesintisi","elektrik kesintisi","trafik düzenlemesi","trafik duzenlemesi",
+        "sahil","feribot","iskele","otogar","marmara","kocaeli","izmit","gebze",
+        "bursa","nilüfer","nilufer","osmangazi","yıldırım","yildirim","istanbul","ibb","imar", "altyapı", "altyapi","su kesintisi", "elektrik kesintisi","yerel etkinlik", "festival", "panayır"],
+    "gundem": ["bakan", "meclis", "cumhurbaşkanı", "seçim", "son dakika","gündem","gundem","meclis","bakan","bakanlık","bakanlik","valilik","belediye",
+        "seçim","secim","mahkeme","yargı","yargi","emniyet","operasyon","gözaltı","gozalti","jandarma","Jandarma"
+        "deprem","yangın","yangin","sel","fırtına","firtina","kaza","soruşturma","sorusturma","saldırı","teror","terör","terörist","acil","gelişme","gelisme",
+        "açıklama","aciklama","duyurdu","duyuru","karar alındı","karar","resmi gazete","bakanlık","bakanlik","tbmm","komisyon","toplantı","toplanti","basın açıklaması","yasa","kanun","yönetmelik","yonetmelik"],
+    "dunya": ["ukraine", "israel", "gaza", "usa", "china", "russia", "world","international","global","iran","europe",
+        "nato","united nations","g7","g20","sanction","ceasefire","election","summit","conflict","war","afrika","africa","dünya","dunya","uluslararası","uluslararasi","yurt dışı","yurtdışı","yurtdisi",
+        "abd","amerika","beyaz saray","pentagon","avrupa","ab","avrupa birliği","bm","birleşmiş milletler","palastine","seçim","referandum","election","referendum"],
+    "spor": ["maç", "transfer", "gol", "lig","spor","mac","süper lig","super lig","şampiyona","sampiyona","hakem",
+        "derbi","futbol","basketbol","voleybol","tenis","formula 1","box","uefa","şampiyonlar ligi","avrupa ligi","spor","maç","mac ","müsabaka","musabaka","karşılaşma","karsilasma",
+        "lig","puan durumu","fikstür","fikstur","play-off","playoff","transfer","bonservis","sözleşme","sozlesme","imza attı","imza","teknik direktör","td","antrenör","antrenor"],
+    "ekonomi": ["enflasyon", "dolar", "borsa", "faiz", "ekonomi","ekonomik","bütçe","butce","vergi","zam","asgari ücret","asgari ucret","memur maaşı","memur maasi","büyüme","daralma","resesyon","cari açık","indirim",
+        "ihracat","ithalat","üretim","uretim","sanayi","istihdam","işsizlik","issizlik","kobi","ticaret","turizm geliri","dış ticaret","politika faizi","tcmb","merkez bankası"],
+    "teknoloji": ["yapay zeka", "ai", "apple", "google","yazılım","yazilim","uygulama","güncelleme","guncelleme","telefon","akıllı","akilli","teknoloji", "dijital",
+        "machine learning","kod", "programlama","uygulama", "app","donanım", "donanim","çip", "cip", "işlemci", "islemci","android", "ios", "iphone","microsoft",
+        "siber", "siber güvenlik","internet", "veri", "bulut","android","robot","siber","hack","sızıntı","sizinti","openai","google","samsung","tesla"],
+   "finans": [ "borsa","bist","hisse","endeks","temettü","temettu","bedelsiz","bedelli","halka arz","tahvil","bono","eurobond","mevduat","fon","tefas","portföy","portfoy","dolar","euro","sterlin","kur","parite","altın","altin","gram altın","ons",
+        "faiz","politika faizi","enflasyon","tcmb","fed","ppk","swap","cds","resesyon",
+        "kripto","bitcoin","ethereum","btc","eth","emtia","petrol","brent","wti","doğalgaz",
+        "nasdaq","s&p","dow","market","markets","stocks","shares","bond","inflation","rates","finans", "finansal", "piyasa", "piyasalar"],
+    "saglik": ["sağlık","saglik","hastane","doktor","virüs","virus","enfeksiyon","kanser","tedavi","ilaç","ilac","sağlık", "saglik", "tıp", "tip",
+     "hastane", "doktor", "hekim", "klinik", "hastalık", "hastalik", "tanı", "tani","psikiyatri","beslenme", "sağlık bakanlığı", "aşı","asi","covid","koronavirüs","koronavirus", "grip","diyet","obezite","psikoloji","ruh sağlığı","ruh sagligi"],
+   "bilim": ["bilim","araştırma","arastirma","deney","laboratuvar","keşif","kesif","fizik","kimya","biyoloji","genetik","esa",
+        "astronomi","nasa","teleskop","uzay","roket","iklim","deprem araştırması","jeoloji", "bilim", "bilimsel", "araştırma", "arastirma","bilim insanı","akademik"],
+    "savunma": ["savunma","savunma sanayii","ordu","asker","tatbikat","füze","fuze","tank","savaş uçağı","savas ucagi",
+       "hava kuvvetleri","deniz kuvvetleri","operasyon","terör","teror","saldırı","saldiri","nato","güvenlik","guvenlik","savunma", "savunma sanayii",
+        "askeri", "ordu", "asker","operasyon", "tatbikat","silah", "füze", "fuze","hava kuvvetleri", "deniz kuvvetleri","savaş uçağı", "tank", "zırhlı", "zirhli","terör", "teror", "güvenlik",
+       "nato", "askeri üs","iha","siha","dron","drone"],
+    "oyun": ["oyun","video oyunu","bilgisayar oyunu","playstation","ps4","ps5","xbox","nintendo","switch","steam","fps",
+        "epic games","mobil oyun","battle royale","e-spor","espor","gamer","gaming","call of duty","pubg",
+        "fortnite","valorant","cs2","counter strike","league of legends","lol","dota","gta","fifa","efootball"],
+    "otomobil": ["otomobil","araba","araç","arac","otomotiv","sedan","suv","tesla","bmw","mercedes","renault","hyundai","Fiat","Ferrari","Lamborghini","Alfa-Romeo","Maserati","Lancia","GMC","Lincoln","Ford","Tesla","Chrysler","Jeep","RAM",
+        "togg","elektrikli","şarj","sarj","menzil","trafik","otoyol","muayene","lastik","kaza","airbag","Toyota","Suzuki","Mitsubishi","Honda","Subaru","Nissan","Acura","Isuzu","Infiniti","Mazda","Lexus",
+        "Hyundai","Kia","Skoda","Dacia","Seat","Lada","Volvo","Audi","BMW","Mercedes","Benz","Mini","Opel","Porsche","Smart","Volkswagen","Renault","Peugeot","Citroen","Bugatti","DS Automobiles","Alpine",
+        "motor", "şanzıman","sanziman","trafik","kaza","otoyol","ehliyet","muayene","sigorta"],
+    "yasam": ["yaşam","yasam","hayat","aile","çocuk","cocuk","evlilik","ilişki","iliski","alışveriş","alisveris","günlük hayat", "gunluk hayat","aile", "çocuk", "cocuk","ev", "ev yaşamı", "ev hayati",
+        "seyahat", "tatil", "gezi","alışveriş", "alisveris","yemek", "tarif", "mutfak","dekorasyon", "hobi","kişisel gelişim", "kisisel gelisim",
+        "tatil","seyahat","gezi","yemek","tarif","dekorasyon","hobi","günlük","gunluk","psikoloji","eğitim","egitim"],
+    }
 
 def clean_html(text):
     if not text:
@@ -38,10 +77,19 @@ def normalize_summary(summary, title, source_type):
     if summary and len(summary) > 80:
         return summary
 
-    if source_type == "intl":
-        return f"{title} başlıklı haber yabancı kaynaklardan derlenmiştir. Ayrıntılar haber içeriğinde yer almaktadır."
+if source_type == "intl":
+    category = detect_intl_category(combined)
+else:
+    category = detect_category(combined)
+    if is_local(combined):
+        category = "yerel"
 
-    return f"{title} ile ilgili gelişmeler haber detaylarında yer alıyor."
+def detect_intl_category(text):
+    t = text.lower()
+    for cat, keys in INTL_CATEGORY_KEYWORDS.items():
+        if any(k in t for k in keys):
+            return cat
+    return "dunya"
 
 def detect_category(text):
     t = text.lower()
